d0591a5c13dd10c1b2b8dfd1de5ec9de9eed013d
Jecricho Knox <hjimmyk@163.com>
2025-09-28 21:06:10 +0800

    创建hint.hpp核心库，创建BigInt，BigFrac

---
+++ main.cpp
@@ -0,0 +1,187 @@
+﻿#include <vector>
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <cmath>
+#include <fstream>
+#include "src/hint.hpp"
+
+// 生成固定长度的随机整数向量
+std::vector<uint64_t> generateRandomIntVector(size_t length, uint64_t min_val = 0, uint64_t max_val = UINT64_MAX)
+{
+	std::vector<uint64_t> vec;
+	vec.reserve(length);
+
+	unsigned						   seed = 120;
+	std::default_random_engine		   generator(seed);
+	std::uniform_int_distribution<uint64_t> distribution(min_val, max_val);
+
+	for (int i = 0; i < length; ++i)
+	{
+		vec.push_back(distribution(generator));
+	}
+
+	return vec;
+}
+
+double test_time(size_t bits) {
+	using namespace HyperInt::Arithmetic::Numeral;
+	const size_t base = 10;
+	const double		  base_d = BaseTable::table2[base - 2];
+	const uint64_t		  base_num = BaseTable::table1[base - 2][0];
+
+	using namespace HyperInt::Arithmetic;
+	size_t len = (1ull << bits);
+	std::vector<uint64_t> a(len, UINT64_MAX);
+	static std::mt19937 gen(1);
+	std::uniform_int_distribution<uint64_t> dist(0, UINT64_MAX);
+	for (auto&& i : a) {
+		i = dist(gen);
+	}
+
+	std::vector<uint64_t> c(get_buffer_size(len, base_d), 0);
+	auto t1 = std::chrono::steady_clock::now();
+	size_t len2 = num2base(a.data(), len, base, c.data());
+	auto t2 = std::chrono::steady_clock::now();
+
+	std::chrono::duration<double> time_span = t2 - t1;
+	return time_span.count();
+}
+
+auto test_time_len(size_t len) {
+	using namespace HyperInt::Arithmetic::Numeral;
+	const size_t base = 10;
+	const double		  base_d = BaseTable::table2[base - 2];
+	const uint64_t		  base_num = BaseTable::table1[base - 2][0];
+
+	using namespace HyperInt::Arithmetic;
+	std::vector<uint64_t> a(len, UINT64_MAX);
+	static std::mt19937 gen(1);
+	std::uniform_int_distribution<uint64_t> dist(0, UINT64_MAX);
+	for (auto&& i : a) {
+		i = dist(gen);
+	}
+
+	std::vector<uint64_t> c(get_buffer_size(len, base_d), 0);
+	auto t1 = std::chrono::steady_clock::now();
+	size_t len2 = num2base(a.data(), len, base, c.data());
+	auto t2 = std::chrono::steady_clock::now();
+
+	return std::chrono::duration<double>(t2 - t1).count();
+}
+
+void test1() {
+	using namespace HyperInt::Arithmetic::Numeral;
+	size_t len = 123 + (1ull << 13);
+	const double		  base_d = BaseTable::table2[8];
+	const uint64_t		  base_num = BaseTable::table1[8][0];
+	size_t				  len_new = std::ceil(base_d * len) + 1;
+
+	std::vector<uint64_t> vec1 = generateRandomIntVector(len);
+	std::vector<uint64_t> vec2(len, 0);
+	std::copy(vec1.begin(), vec1.end(), vec2.begin());
+
+	std::vector<uint64_t> res2(len_new, 0);
+
+	std::vector<uint64_t> res1(len_new, 0);
+	size_t len1 = num2base_classic(vec1.data(), len, base_num, res1.data());
+	std::cout << "len1: " << len1 << std::endl;
+	for (size_t i = len1 - 20; i < len1; i++)
+	{
+		std::cout << res1[i] << " ";
+	}
+	std::cout << std::endl << std::endl;
+
+	auto t1 = std::chrono::steady_clock::now();
+	size_t len2 = num2base(vec2.data(), len, 10, res2.data());
+	auto t2 = std::chrono::steady_clock::now();
+	for (size_t i = len2 - 20; i < len2; i++)
+	{
+		std::cout << res2[i] << " ";
+	}
+	std::cout << std::endl << std::endl;
+	std::chrono::duration<double> time_span = t2 - t1;
+	std::cout << "len2: " << len2 << std::endl;
+	std::cout << "time: " << time_span.count() << std::endl;
+
+}
+
+void test2() {
+	using namespace HyperInt::Arithmetic::Numeral;
+	const double		  base_d = BaseTable::table2[8];
+	const uint64_t		  base_num = BaseTable::table1[8][0];
+	size_t max_len = 1ull << 10;
+	_2pow64_index_list index_list = create_2pow64_index_list(max_len, base_num, base_d);
+	_2pow64_index_list current = index_list;
+
+	while (current != nullptr) {
+		std::cout << "length: " << current->length << std::endl;
+		std::cout << "index: " << current->index << std::endl;
+		for (size_t i = 0; i < current->length; i++)
+		{
+			std::cout << current->_2pow64_index[i] << " ";
+		}
+		std::cout << std::endl << std::endl;
+		current = current->back;
+	}
+
+
+}
+
+void test_time_csv1(int begin, int end, int step) {
+
+	std::vector<size_t> inputLengths;
+	std::vector<double> runtimes;
+	for (int i = begin; i <= end; i += step) {
+		size_t len = i;
+		double time = test_time_len(len);
+		std::cout << "len: " << len << std::endl;
+		std::cout << "time: " << time << std::endl;
+		inputLengths.push_back(len * 64);
+		runtimes.push_back(time);
+	}
+
+	std::ofstream file("time.csv");
+	if (!file.is_open()) {
+		std::cerr << "错误：无法打开文件 " << std::endl;
+		return;
+	}
+
+	file << "输入长度,运行时间(秒)" << std::endl;
+
+	for (size_t i = 0; i < inputLengths.size(); ++i) {
+		file << inputLengths[i] << "," << runtimes[i] << std::endl;
+	}
+	file.close();
+}
+void test_time_csv2(int begin, int end) {
+
+	std::vector<size_t> inputLengths;
+	std::vector<double> runtimes;
+	for (int i = begin; i <= end; i++) {
+		size_t len = 1ull << i;
+		double time = test_time_len(len);
+		inputLengths.push_back(len);
+		runtimes.push_back(time);
+	}
+
+	std::ofstream file("time.csv");
+	if (!file.is_open()) {
+		std::cerr << "错误：无法打开文件 " << std::endl;
+		return;
+	}
+
+	file << "输入长度,运行时间(秒)" << std::endl;
+
+	for (size_t i = 0; i < inputLengths.size(); ++i) {
+		file << inputLengths[i] << "," << runtimes[i] << std::endl;
+	}
+	file.close();
+}
+
+
+
+int main() {
+	test1();
+	return 0;
+}
---
+++ src/BigFrac.hpp
@@ -0,0 +1,277 @@
+﻿/*
+ * [LAMINA_SCI_CAL]
+ * Copyright (C) [2025] [HJimmyK/LAMINA]
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+/*
+MIT License
+
+Copyright (c) 2024-2050 Twilight-Dream & With-Sky
+
+https://github.com/Twilight-Dream-Of-Magic/
+https://github.com/With-Sky
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef LAMINA_BIG_FRACTION_HPP
+#define LAMINA_BIG_FRACTION_HPP
+
+#include "BigInt.hpp"
+
+namespace __LAMINA::BIGFRAC
+{
+	enum class DecimalPrecisionMode : uint32_t
+	{
+		Fixed = 0,	 // Specify the precision mode
+		Full = 1	 // Full the precision mode
+	};
+
+	inline const __LAMINA::BIGINT::BigInteger ONE = 1;
+	inline const __LAMINA::BIGINT::BigInteger TWO = 2;
+	inline const __LAMINA::BIGINT::BigInteger THREE = 3;
+	inline const __LAMINA::BIGINT::BigInteger FIVE = 5;
+	inline const __LAMINA::BIGINT::BigInteger TEN = 10;
+	inline const __LAMINA::BIGINT::BigInteger RADIX = TEN;
+
+	class BigFraction
+	{
+	public:
+		DecimalPrecisionMode PrecisionMode = DecimalPrecisionMode::Fixed;
+		uint64_t FixedPrecisionCount = 2;
+
+		using BigInteger = __LAMINA::BIGINT::BigInteger;
+		using BigSignedInteger = __LAMINA::BIGINT::BigSignedInteger;
+
+		BigFraction();
+		BigFraction(const BigFraction& other) noexcept;
+		BigFraction(BigFraction&& other) noexcept;
+		explicit BigFraction(const BigInteger& numerator);
+		BigFraction(const BigInteger& numerator, const BigInteger& denominator);
+		BigFraction(const BigInteger& numerator, const BigInteger& denominator, int32_t sign);
+		explicit BigFraction(const std::string& complexString);
+
+		void	   SetSimplifyReduced(bool value);
+		bool	   IsNaN() const;
+		bool	   IsInfinityPoint() const;
+		bool	   IsZero() const;
+		bool	   IsNegative() const;
+		bool	   IsInteger() const;
+		BigInteger GetNumerator() const;
+		BigInteger GetDenominator() const;
+		std::optional<BigInteger> TryGetInteger() const;
+		// 设置分子
+		void	   SetNumerator(const BigInteger& number);
+		// 设置分母
+		void	   SetDenominator(const BigInteger& number);
+		BigFraction GetFullPrecision() const;
+		void SetFullPrecision(BigInteger number);
+
+		void		ComputeAndFromDecimalString(const std::string& complexString);
+		std::string ComputeAndToDecimalString() const;
+
+		BigFraction& operator=(const BigFraction& other);
+		BigFraction& operator=(BigFraction&& other);
+		BigFraction& operator+=(const BigFraction& other);
+		BigFraction& operator+=(const BigInteger& other);
+		BigFraction& operator-=(const BigFraction& other);
+		BigFraction& operator-=(const BigInteger& other);
+		BigFraction& operator*=(const BigFraction& other);
+		BigFraction& operator*=(const BigInteger& other);
+		BigFraction& operator/=(const BigFraction& other);
+		BigFraction& operator/=(const BigInteger& other);
+
+		BigFraction operator+(const BigFraction& other) const;
+		BigFraction operator+(const BigInteger& other) const;
+		BigFraction operator-(const BigFraction& other) const;
+		BigFraction operator-(const BigInteger& other) const;
+		BigFraction operator*(const BigFraction& other) const;
+		BigFraction operator*(const BigInteger& other) const;
+		BigFraction operator/(const BigFraction& other) const;
+		BigFraction operator/(const BigInteger& other) const;
+
+		BigFraction operator-() const;
+
+		bool operator<(const BigFraction& other) const;
+		bool operator<=(const BigFraction& other) const;
+		bool operator>(const BigFraction& other) const;
+		bool operator>=(const BigFraction& other) const;
+		bool operator==(const BigFraction& other) const;
+		bool operator!=(const BigFraction& other) const;
+
+		BigFraction Abs() const;
+		BigFraction Reciprocal() const;
+		BigFraction Sqrt();
+		BigFraction Cbrt();
+		BigFraction Log(const BigInteger& value) const;
+		BigFraction Log() const;
+		BigFraction Log10(const BigInteger& fraction) const;
+		BigFraction Log10() const;
+		// nth Power of a BigFraction
+		BigFraction Power(const BigInteger& exponent) const;
+		BigFraction Power(const BigFraction& exponent) const;
+		// nth Root of a BigFraction
+		BigFraction NthRoot(const BigInteger& n) const;
+		BigFraction NthRoot(const BigFraction& fraction, const BigInteger& n) const;
+		BigFraction Sine(const BigFraction& x) const;
+		BigFraction Cosine(const BigFraction& x) const;
+		BigFraction Tangent(const BigFraction& x) const;
+		BigFraction Arctangent(const BigFraction& x) const;
+		BigFraction Arcsine(const BigFraction& x) const;
+		BigFraction Arccosine(const BigFraction& x) const;
+
+		BigInteger	Floor() const;
+		BigInteger	Ceil() const;
+		BigInteger	Round() const;
+
+		template <typename FloatingType>
+		BigFraction FromFloatingNumber(FloatingType value) const
+		{
+			BigFraction result;
+
+			if (std::isnan(value))
+			{
+				result.sign = 1;
+				result.numerator = 0;
+				result.denominator = ONE;
+				return result;
+			}
+
+			if (std::isinf(value))
+			{
+				result.sign = value < 0 ? -1 : 1;
+				result.denominator = 0;
+				result.numerator = ONE;
+				return result;
+			}
+
+			result.sign = value < 0 ? -1 : 1;
+			value = std::fabs(value);
+
+			FloatingType integerPart;
+			FloatingType fractionPart = std::modf(value, &integerPart);
+
+			result.numerator = static_cast<BigInteger>(integerPart);
+			result.denominator = ONE;
+
+			switch (this->PrecisionMode)
+			{
+			case DecimalPrecisionMode::Fixed:
+			{
+				for (uint64_t round = this->FixedPrecisionCount; round > 0 && fractionPart != 0.0; --round)
+				{
+					fractionPart *= 10;
+					FloatingType newIntPart;
+					fractionPart = std::modf(fractionPart, &newIntPart);
+					result.numerator = result.numerator * TEN + static_cast<BigInteger>(newIntPart);
+					result.denominator *= TEN;
+				}
+				break;
+			}
+
+			case DecimalPrecisionMode::Full:
+			{
+				// Convert the precision to a floating-point number for comparison
+				FloatingType floatingPrecision = static_cast<FloatingType>(GetFullPrecision());
+
+				while (true)
+				{
+					fractionPart *= 10;
+					FloatingType newIntPart;
+					fractionPart = std::modf(fractionPart, &newIntPart);
+					result.numerator = result.numerator * TEN + static_cast<BigInteger>(newIntPart);
+					result.denominator *= TEN;
+
+					// Convert current fractional difference to floating-point and compare
+					if (std::fabs(fractionPart) < floatingPrecision)
+					{
+						break;
+					}
+				}
+				break;
+			}
+
+			default:
+				throw std::invalid_argument("Unknown PrecisionMode");
+			}
+
+			if (simplify_reduced)
+			{
+				result.ReduceSimplify();
+			}
+
+			return result;
+		}
+
+		long double BigIntegerToLongDouble(const BigInteger& big_integer) const;
+		operator BigInteger() const;
+		operator long double() const;
+		operator double() const;
+		operator float() const;
+
+		//生成由拉马努金提出的 π 的高精度表示。
+		static BigFraction GenerateSrinivasaRamanujanPI();
+		//用于通过 Nilakantha 数列计算 π 的近似值，迭代次数由参数 iteration 指定。
+		static BigFraction GenerateNilakanthaArrayPI(uint64_t iteration);
+		// 输入参数为：
+		//  • const BigInteger& value：要计算指数的整数值（即 e ^ value）。
+		//  • DecimalPrecisionMode precision_mode（可选，默认值为 Full）：指定计算精度模式（定点或全精度）。
+		//  • uint64_t fixed_precision_count（可选，默认值为 2）：在定点模式下指定泰勒展开的项数。
+		// 输出为：
+		//  • 返回值类型为 BigFraction，表示 e ^ value 的高精度分数近似值。
+		static BigFraction Exponential_Taylor(const BigInteger& value, DecimalPrecisionMode precision_mode = DecimalPrecisionMode::Full, uint64_t fixed_precision_count = 2);
+		static BigFraction Exponential_Taylor(const BigFraction& value);
+		static BigFraction Exponential(const BigFraction& x, int64_t precision_digits = 16);
+
+		friend std::istream& operator>>(std::istream& is, BigFraction& fraction);
+		friend std::ostream& operator<<(std::ostream& os, const BigFraction& fraction);
+
+		friend BigFraction operator+(const BigInteger& left, const BigFraction& right);
+		friend BigFraction operator-(const BigInteger& left, const BigFraction& right);
+		friend BigFraction operator*(const BigInteger& left, const BigFraction& right);
+		friend BigFraction operator/(const BigInteger& left, const BigFraction& right);
+
+	private:
+		BigInteger numerator;
+		BigInteger denominator;
+		int32_t	   sign;
+		bool	   simplify_reduced = true;
+
+		void ReduceSimplify();
+		BigFraction LogarithmCF(const BigInteger& value) const;
+		BigFraction LogarithmHalley(const BigInteger& a) const;
+	};
+
+	inline BigFraction BigFractionFullPrecision(1, 0);
+
+}  // namespace __BIGFRAC
+
+#endif
